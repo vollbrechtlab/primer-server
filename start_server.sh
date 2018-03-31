44e235ad59a461b2c056cb724a830dcbd65f2b15
Takao Shibamoto <takao8@gmx.com>
2018-03-31 18:54:13 -0500

    small change

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd v$1
+cd /rest-api/v$1
 venv/bin/gunicorn -b 0.0.0.0:8001 --workers=2 --access-logfile logs/gunicorn-access.log --error-logfile logs/gunicorn-error.log server:app --daemon --name=primer-server-rest-api-deamon-v$1
-cd ..
+cd ../../
