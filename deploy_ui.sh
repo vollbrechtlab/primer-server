20871c611e52cea27946440804ca219dfb5974bf
Kokulapalan Wimalanathan <kokul@iastate.edu>
2018-04-03 10:51:03 -0500

    added lines to modify .htaccess with each deployment

--- deploy_ui.sh
+++ deploy_ui.sh
@@ -1,7 +1,7 @@
 if pushd /var/www/html/primer-server; then
     echo "/var/www/html/primer-server exists"
     popd
-else 
+else
 	echo "/var/www/html/primer-server doesn't exist"
 	echo "making /var/www/html/primer-server"
 	mkdir /var/www/html/primer-server
@@ -10,3 +10,6 @@ fi
 echo "copying ui/v$1 to /var/www/html/primer-server/v$1"
 rm -R /var/www/html/primer-server/v$1
 cp -R ui/v$1 /var/www/html/primer-server/v$1
+
+echo "RewriteEngine on" > /var/www/html/.htaccess
+echo "RewriteRule ^primer-server/?$ http://18.219.153.20/primer-server/v$1/ [R=302,L]" >> /var/www/html/.htaccess
