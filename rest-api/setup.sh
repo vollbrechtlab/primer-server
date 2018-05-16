19f81aaa60a21cb665b22d8bcc5dc381e131c00f
Kokulapalan Wimalanathan <kokul@iastate.edu>
2018-05-16 10:36:58 -0500

    added setup scripts for rest api

---
+++ rest-api/setup.sh
@@ -0,0 +1,10 @@
+##Installing primerDAFT and primerDAFT requirements
+git clone git@github.com:vollbrechtlab/primerDAFT.git
+cd primerDAFT
+pip3 install -r requirements.txt
+python setup.py
+
+
+##Installing primer-server requirements
+cd ..
+pip3 install -r requirements.txt
